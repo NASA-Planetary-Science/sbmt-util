665107dceedb16577147588698cf94556939053b
nguyel1 <nguyel1@NGUYEL1-WL3.dom1.jhuapl.edu>
2016-10-12 15:09:32 -0400

    Committing missed file from recent merge

---
+++ src/edu/jhuapl/sbmt/util/BackplanesLabel.java
@@ -0,0 +1,9 @@
+package edu.jhuapl.sbmt.util;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface BackplanesLabel
+{
+    public void generateBackplanesLabel(File imgName, File lblFileName) throws IOException;
+}
