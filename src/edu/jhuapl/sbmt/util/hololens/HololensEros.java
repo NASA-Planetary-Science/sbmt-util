890fd2f57585fc8c7800bc5f40bf98bd1285d729
Mike Zimmerman <Michael.Zimmerman@jhuapl.edu>
2017-01-12 11:53:23 -0500

    Changed the approach for outputting colormapped data for Hololens. Now it writes the colormap to a 1D texture and assigns texture coordinates to the vertices based upon their respective data values scaled to the interval [0,1]. Previously it had used vertex colors but that was incompatible with Unity without making significant changes to the shader.

--- src/edu/jhuapl/sbmt/util/hololens/HololensEros.java
+++ src/edu/jhuapl/sbmt/util/hololens/HololensEros.java
@@ -1,15 +1,23 @@
 package edu.jhuapl.sbmt.util.hololens;
 
-import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.imageio.ImageIO;
+
+import vtk.vtkCellDataToPointData;
+import vtk.vtkDoubleArray;
+import vtk.vtkFloatArray;
 import vtk.vtkNativeLibrary;
+import vtk.vtkPNGReader;
 import vtk.vtkPolyData;
-import vtk.vtkUnsignedCharArray;
+import vtk.vtkPolyDataWriter;
 
 import edu.jhuapl.saavtk.colormap.Colormap;
 import edu.jhuapl.saavtk.colormap.Colormaps;
+import edu.jhuapl.saavtk.model.ColoringInfo;
 import edu.jhuapl.saavtk.model.ShapeModelAuthor;
 import edu.jhuapl.saavtk.model.ShapeModelBody;
 import edu.jhuapl.saavtk.util.ObjUtil;
@@ -28,13 +36,65 @@ public class HololensEros
 
     public static void main(String[] args) throws IOException
     {
-        int coloringIndex=2; // at the time of implementation index 1 is point-centered elevation data
+        int coloringIndex=2; // at the time of implementation index 2 is point-centered elevation data
 
         SmallBodyViewConfig config=SmallBodyViewConfig.getSmallBodyConfig(ShapeModelBody.EROS, ShapeModelAuthor.GASKELL);
         SmallBodyModel erosModel = SbmtModelFactory.createSmallBodyModel(config);
         erosModel.setColoringIndex(coloringIndex);
+        double[] coloringRange=erosModel.getCurrentColoringRange(coloringIndex);
 
         Colormap colormap=Colormaps.getNewInstanceOfBuiltInColormap("Spectral_lowBlue");
+        colormap.setRangeMin(0);
+        colormap.setRangeMax(1);
+        BufferedImage image=new BufferedImage(1, colormap.getNumberOfLevels(), BufferedImage.TYPE_INT_RGB);
+        for (int i=0; i<colormap.getNumberOfLevels(); i++)
+        {
+            double lev=(double)i/(double)(colormap.getNumberOfLevels()-1);
+            image.setRGB(0, i, colormap.getColor(lev).getRGB());
+        }
+        Path pngPath=Paths.get("/Users/zimmemi1/Desktop/Spectral_lowBlue.png");
+        ImageIO.write(image, "png", pngPath.toFile());
+
+        vtkPNGReader reader=new vtkPNGReader();
+        reader.SetFileName(pngPath.toString());
+        reader.Update();
+
+        ColoringInfo info=erosModel.getColoringInfoList().get(coloringIndex);
+        vtkDoubleArray values=new vtkDoubleArray();
+        values.SetName("values");
+        for (int i=0; i<erosModel.getSmallBodyPolyData().GetNumberOfCells(); i++)
+        {
+            double scaledValue=(info.coloringValues.GetTuple1(i)-coloringRange[0])/(coloringRange[1]-coloringRange[0]);
+            if (scaledValue<0)
+                scaledValue=0;
+            if (scaledValue>1)
+                scaledValue=1;
+            values.InsertNextValue(scaledValue);
+        }
+        erosModel.getSmallBodyPolyData().GetCellData().AddArray(values);
+
+        vtkCellDataToPointData converter=new vtkCellDataToPointData();
+        converter.SetInputData(erosModel.getSmallBodyPolyData());
+        converter.Update();
+
+
+        vtkPolyData polyData=converter.GetPolyDataOutput();
+        vtkFloatArray texCoords=new vtkFloatArray();
+        texCoords.SetNumberOfComponents(2);
+        vtkDoubleArray pointValues=(vtkDoubleArray)polyData.GetPointData().GetArray("values");
+        for (int i=0; i<polyData.GetNumberOfPoints(); i++)
+            texCoords.InsertNextTuple2(0,pointValues.GetValue(i));
+        erosModel.getSmallBodyPolyData().GetPointData().SetTCoords(texCoords);
+        ObjUtil.writePolyDataToObj(erosModel.getSmallBodyPolyData(), reader.GetOutput(), Paths.get("/Users/zimmemi1/Desktop/erosWithElevationTexCoords.obj"), "");
+
+
+        vtkPolyDataWriter writer=new vtkPolyDataWriter();
+        writer.SetFileName("/Users/zimmemi1/Desktop/test.vtk");
+        writer.SetFileTypeToBinary();
+        writer.SetInputData(erosModel.getSmallBodyPolyData());
+        writer.Write();
+
+/*        Colormap colormap=Colormaps.getNewInstanceOfBuiltInColormap("Spectral_lowBlue");
         double[] coloringRange=erosModel.getCurrentColoringRange(coloringIndex);
         colormap.setRangeMin(coloringRange[0]);
         colormap.setRangeMax(coloringRange[1]);
@@ -54,7 +114,35 @@ public class HololensEros
         polyData.GetPointData().AddArray(colors);
         polyData.GetPointData().SetScalars(colors);
 
-        ObjUtil.writePolyDataToObj(polyData, Paths.get("/Users/zimmemi1/Desktop/test.obj"));
+        ObjUtil.writePolyDataToObj(polyData, Paths.get("/Users/zimmemi1/Desktop/test.obj"));*/
+
+/*        Path elevationFile=Paths.get("/Users/zimmemi1/Desktop/erosElevationScaled.txt");
+        FileWriter writer=new FileWriter(elevationFile.toFile());
+        ColoringInfo info=erosModel.getColoringInfoList().get(coloringIndex);
+        writer.write("# values = "+info.coloringValues.GetNumberOfTuples()+"\n");
+        for (int i=0; i<info.coloringValues.GetNumberOfTuples(); i++)
+        {
+            double scaledValue=(info.coloringValues.GetTuple1(i)-coloringRange[0])/(coloringRange[1]-coloringRange[0]);
+            if (scaledValue<0)
+                scaledValue=0;
+            if (scaledValue>1)
+                scaledValue=1;
+            writer.write(scaledValue+"\n");
+        }
+        writer.close();
+
+        Path colormapFile=Paths.get("/Users/zimmemi1/Desktop/colormap_Spectral_lowBlue.txt");
+        colormap.setRangeMin(0);
+        colormap.setRangeMax(1);
+        FileWriter writer2=new FileWriter(colormapFile.toFile());
+        writer2.write("colors = "+colormap.getNumberOfLevels()+"\n");
+        for (int i=0; i<colormap.getNumberOfLevels(); i++)
+        {
+            Color c=colormap.getColor((double)i/(double)(colormap.getNumberOfLevels()-1));
+            writer2.write(c.getRed()+" "+c.getGreen()+" "+c.getBlue()+"\n");
+        }
+        writer2.close();*/
+
 
     }
 
