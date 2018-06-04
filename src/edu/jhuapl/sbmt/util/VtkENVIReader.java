7cc21fe222037fddef7b3465b0d20a0d9774c8a2
zimmemi1 <Michael.Zimmerman@jhuapl.edu>
2018-06-04 12:20:01 -0400

    Moved FillDetector and ImageDataUtil to saavtk, and fixed compile errors due to merge

--- src/edu/jhuapl/sbmt/util/VtkENVIReader.java
+++ src/edu/jhuapl/sbmt/util/VtkENVIReader.java
@@ -13,6 +13,8 @@ import java.util.HashMap;
 import vtk.vtkImageData;
 import vtk.vtkImageReader2;
 
+import edu.jhuapl.saavtk.util.ImageDataUtil;
+
 // This class was written to be used like vtkPNGReader but
 // only for converting ENVI format files to vtkImageData
 // Note: ENVI format image = binary + .hdr file
