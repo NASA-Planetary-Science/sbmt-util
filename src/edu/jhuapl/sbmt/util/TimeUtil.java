33ec2d6411d0d7b563e2399fcf651de32d9f2e72
lopeznr1 <lopeznr1@jhuapl.edu>
2019-05-24 08:09:20 -0400

    Added method to transform a string to ephemeris.
    A method was added to transform a string to ephemeris time or throw the
    actual exception. Previously -Double.MIN_VALUE was returned which is not
    ideal. Auto formatted code.

--- src/edu/jhuapl/sbmt/util/TimeUtil.java
+++ src/edu/jhuapl/sbmt/util/TimeUtil.java
@@ -14,66 +14,77 @@ import spice.basic.SpiceErrorException;
  */
 public class TimeUtil
 {
-    static
-    {
-        System.loadLibrary("JNISpice");
-        try
-        {
-            File lskFile = ConvertResourceToFile.convertResourceToRealFile(
-                    TimeUtil.class,
-                    "/edu/jhuapl/sbmt/data/naif0010.tls",
-                    Configuration.getApplicationDataDir());
+	static
+	{
+		System.loadLibrary("JNISpice");
+		try
+		{
+			File lskFile = ConvertResourceToFile.convertResourceToRealFile(TimeUtil.class,
+					"/edu/jhuapl/sbmt/data/naif0010.tls", Configuration.getApplicationDataDir());
 
-            CSPICE.furnsh(lskFile.getAbsolutePath());
-        }
-        catch (SpiceErrorException e)
-        {
-            e.printStackTrace();
-        }
-    }
+			CSPICE.furnsh(lskFile.getAbsolutePath());
+		}
+		catch (SpiceErrorException e)
+		{
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Convert UTC string to ephemeris time. If the string is not a valid UTC
-     * string then -Double.MIN_VALUE is returned.
-     *
-     * @param str UTC string, e.g. 2000-04-06T01:44:28.165
-     * @return ephemeris time as a double
-     */
-    public static double str2et(String str)
-    {
-        try
-        {
-            if (str.length() > 23)
-                return CSPICE.str2et(str.substring(0, 23));
-            else
-                return CSPICE.str2et(str);
-        }
-        catch (SpiceErrorException e)
-        {
-            e.printStackTrace();
-        }
+	/**
+	 * Returns the ephemeris time (as a double) corresponding to the string aStr.
+	 *
+	 * @param aStr The input ephemeris string. Ex: 2000-04-06T01:44:28.165
+	 * @return
+	 * @throws SpiceErrorException if aStr is not a valid ephemeris time.
+	 */
+	public static double str2etA(String aStr) throws SpiceErrorException
+	{
+		if (aStr.length() > 23)
+			return CSPICE.str2et(aStr.substring(0, 23));
+		else
+			return CSPICE.str2et(aStr);
+	}
 
-        return -Double.MIN_VALUE;
-    }
+	/**
+	 * Convert UTC string to ephemeris time. If the string is not a valid UTC
+	 * string then -Double.MIN_VALUE is returned.
+	 *
+	 * @param str UTC string, e.g. 2000-04-06T01:44:28.165
+	 * @return ephemeris time as a double
+	 */
+	public static double str2et(String str)
+	{
+		try
+		{
+			// Delegate
+			return str2etA(str);
+		}
+		catch (SpiceErrorException e)
+		{
+			e.printStackTrace();
+		}
 
-    /**
-     * Convert ephemeris time to UTC string. If an error occurs during the
-     * conversion, the empty string is returned.
-     *
-     * @param et ephemeris time
-     * @return UTC string
-     */
-    public static String et2str(double et)
-    {
-        try
-        {
-            return CSPICE.et2utc(et, "ISOC", 6);
-        }
-        catch (SpiceErrorException e)
-        {
-            e.printStackTrace();
-        }
+		return -Double.MIN_VALUE;
+	}
 
-        return "";
-    }
+	/**
+	 * Convert ephemeris time to UTC string. If an error occurs during the
+	 * conversion, the empty string is returned.
+	 *
+	 * @param et ephemeris time
+	 * @return UTC string
+	 */
+	public static String et2str(double et)
+	{
+		try
+		{
+			return CSPICE.et2utc(et, "ISOC", 6);
+		}
+		catch (SpiceErrorException e)
+		{
+			e.printStackTrace();
+		}
+
+		return "";
+	}
 }
