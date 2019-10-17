069fef97d51efbe576f981a51d1459f9b0dd0cbd
Josh Steele <josh.steele@jhuapl.edu>
2019-10-17 13:55:44 -0400

    starting a new tool to build the info files for NIS

---
+++ src/edu/jhuapl/sbmt/util/nis/NISInfoFileGenerator.java
@@ -0,0 +1,13 @@
+package edu.jhuapl.sbmt.util.nis;
+
+import java.io.File;
+
+public class NISInfoFileGenerator
+{
+
+	public NISInfoFileGenerator(String sourceDirectory, String destinationDirectory, String inputList)
+	{
+		File inputFile = new File(sourceDirectory, inputList);
+	}
+
+}
